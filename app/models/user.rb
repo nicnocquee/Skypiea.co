d4aafb58856e84db7741643e5153c95385b45872
Nico Prananta <nicnocquee@yahoo.com>
2013-08-06 13:35:21 +0900

    secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	VALID_USERNAME_REGEX = /\A[a-z0-9_\-.]{1,50}\z/i
 
 	validates :username, presence: true, format: { with: VALID_USERNAME_REGEX}, uniqueness: { case_sensitive: false }
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  	validates :password, length: { minimum: 6 }
 
   	before_save {
   		self.email = email.downcase
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806040249) do
+ActiveRecord::Schema.define(version: 20130806041207) do
 
   create_table "stickers", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20130806040249) do
     t.boolean  "artist"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
