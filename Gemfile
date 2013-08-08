e5ca21c9494b817e60dc903b746225c52d3eceb8
Nico Prananta <nicnocquee@yahoo.com>
2013-08-08 18:34:57 +0900

    Remove test files so that Guard can run Spork correctly

--- Gemfile
+++ Gemfile
@@ -8,13 +8,18 @@ gem 'bootstrap-sass', '2.3.2.0'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
-  gem 'sqlite3'
-  gem 'rspec-rails'
+  gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', github: 'sporkrb/spork-rails'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.0.0'
   gem 'capybara', '2.1.0'
+  gem 'growl', '1.0.3'
 end
 
 # Use SCSS for stylesheets
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-  
+  let(:base_title){"Skypiea"}
+
   describe "About page" do
     it "should have content 'About'" do
       visit '/static_pages/about'
@@ -10,7 +11,7 @@ describe "Static Pages" do
 
     it "should have the right title" do 
     	visit '/static_pages/about'
-    	expect(page).to have_title('Skypiea | About')
+    	expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -22,7 +23,7 @@ describe "Static Pages" do
 
     it "should have the right title" do 
     	visit '/static_pages/term'
-    	expect(page).to have_title('Skypiea | Terms')
+    	expect(page).to have_title("#{base_title} | Terms")
     end
   end
 end
